2ba31215b7fe301a31b4f6bd7d138445a3949ea2
unlock unknown <unknown1@unlocks-MacBook-Pro.local>
2023-01-23 00:05:59 -0300

    FEAT: add vim vundle

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 brew install scc
 brew install htop
 
-echo "CONFIGURING /Users/$USER/.vimrc"
+echo "CONFIGURING /Users/$USER/.vimrc ... Vundle"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp $(pwd)/.vim/.vimrc /Users/$USER/.vimrc
 
